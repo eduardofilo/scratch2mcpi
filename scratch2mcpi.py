012f469e81c08b8c0b547c19eb8a4f176cff67ea
Eduardo Moreno <eduardo@niubit.es>
2017-11-16 15:56:13 +0100

    Traduzco mensaje conexión Scratch2MCPI.

--- scratch2mcpi.py
+++ scratch2mcpi.py
@@ -238,7 +238,7 @@ def main():
         mc = minecraft.Minecraft.create()
 
         if (s):
-            mc.postToChat("Scratch2MCPI connected to Minecraft Pi.")
+            mc.postToChat("Scratch2MCPI conectado con Minecraft Pi. ¡Vamos a programar!")
             print _("Connected to Scratch")
 
             s.broadcast("hola_minecraft")
